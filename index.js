0fb7c30f740177c809355383a250d44a794cf181
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2022-03-02 04:22:20 -0300

    feat: configure sendMenssage to friendlist

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import SteamUser from 'steam-user'
-import logOnOptions from '../modules/login'
+import logOnOptions from './modules/login.js'
 
 const client = new SteamUser()
 
 client.logOn(logOnOptions)
 
 client.on('loggedOn', () => {
-
-    console.log('Yes! Logged into steam')
-
     client.setPersona(SteamUser.EPersonaState.Online)
-    client.gamesPlayed(440)
+})
+
+client.on('friendRelationship', (steamid, relationship) => {
+    if (relationship === 2) {
+        client.addFriend(steamid)
+        client.chatMessage(steamid, 'Hello there! Thanks for adding me!')
+    }
+})
+
+client.on('friendsList', () => {
+    let friends = Object.keys(client.myFriends).filter(steamId => client.myFriends[steamId] == SteamUser.EFriendRelationship.Friend)
+    friends.forEach(e => {
+        client.chat.sendFriendMessage(e, "Hello, world!")
+    })
+    console.log('All Friends: ' + friends.length)
 })
