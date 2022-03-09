c66fe9048f1e95f826fd67973d5b9ca9d4e2f884
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2022-03-09 02:41:45 -0300

    feat: add send mensage group

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
-import SteamUser from 'steam-user'
-import logOnOptions from './modules/login.js'
+import SteamUser from "steam-user"
+import logOnOptions from "./modules/login.js"
 
 const bot = new SteamUser()
 
 bot.logOn(logOnOptions)
 
-// Logged -> Change status to online
-bot.on('loggedOn', () => {
-    bot.setPersona(SteamUser.EPersonaState.Online)
+// Accept Friend Invite
+bot.on("loggedOn", () => {
+  bot.setPersona(SteamUser.EPersonaState.Online)
 })
 
-// Accept Friend Invite 
-bot.on('friendRelationship', (steamid, relationship) => {
-    if (relationship === 2) {
-        bot.addFriend(steamid)
-        bot.chatMessage(
-            steamid,
-            `
-                Hi! Thanks for adding me! 
-                I'm Price Bot!
-            `
-        )
-    }
+// Accept friend invite
+bot.on("friendRelationship", (steamid, relationship) => {
+  if (relationship === 2) {
+    bot.addFriend(steamid)
+    bot.chatMessage(
+      steamid,
+      `
+        Hi! Thanks for adding me! 
+        I'm Price Bot!
+        Link to Group: https://s.team/chat/D9LnpxMZ
+      
+        `
+    )
+  }
 })
 
-// Global Mensage
-// bot.on('friendsList', () => {
-//     const friends = Object.keys(bot.myFriends).filter(steamId => bot.myFriends[steamId] == SteamUser.EFriendRelationship.Friend)
-//     friends.forEach(e => {
-//         bot.chat.sendFriendMessage(e, "Salve")
-//     })
-//     console.log('All Friends: ' + friends.length)
-// })
-
-// Chat
-bot.on('friendMessage', (steamid, message) => {
-    message = message.toLowerCase()
-    if (message == 'hi') {
-        bot.chat.sendFriendMessage(steamid, "Hi! How are You?")
-    }
+// Chat Friends
+bot.on("friendMessage", (steamid, message) => {
+  message = message.toLowerCase()
+  if (message == "hi") {
+    bot.chat.sendFriendMessage(steamid, "Hi! How are You?")
+  }
 })
 
-bot.on('groupList', () => {
-    console.log(Object.keys(bot.myGroups)[0])
-    bot.chat.sendChatMessage('103582791471570174', '0', "hello")
+// Send mensage group Price Bot
+bot.on("groupList", () => {
+  bot.chat.getClanChatGroupInfo("103582791471570174", function (err, response) {
+    err ? console.log(err.response) : console.log(response.chat_group_summary)
+  })
+
+  bot.chat.sendChatMessage("22328242", "78507305", "hello")
 })
